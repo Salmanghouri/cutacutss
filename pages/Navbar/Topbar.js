7a6b5b3284e2b0420d64ab772b6cdec629f76453
Salman Ghouri (Mark2) <ghourisalman24@gmail.com>
2022-05-10 19:20:10 +0500

    ss5

--- pages/Navbar/Topbar.js
+++ pages/Navbar/Topbar.js
@@ -5,7 +5,7 @@ const Topbar = () => {
   return (
     <div className="relative px-2 py-1 text-white bg-[#E72526] pr-14">
   <p className="text-sm font-medium text-left sm:text-center">
-  FREE SHIPPING 🚚 ON ORDERS OVER $85 IN THE CONTIGUOUS USA
+  FREE SHIPPING 🚚 ON ORDERS OVER $85 IN THE CONTIGUOUS UK
     
   </p>
   <button aria-label="Close" className="absolute p-1 transition -translate-y-1/2 rounded-lg top-1/2 right-4 bg-black/10 hover:bg-black/20">
